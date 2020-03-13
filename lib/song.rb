030ba9ab52e340d275691899fab019b57e50d406
Pharia Le <pharia.le@gmail.com>
2020-03-13 01:26:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,33 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
   def self.all
     @@all
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    name = self.new
+    name
+    # song = self.create
+    # song.name = name
+    # song
+  end
+
+  def self.create_by_name(name)
+    name = new_by_name(name)
+  end
+  
+  def self.find_by_name(name)
+    @@all.find { |song| name }
   end
 
   def save
